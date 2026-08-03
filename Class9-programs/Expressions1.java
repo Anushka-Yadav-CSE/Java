a31755fd3a81e0e1e334ab19ba8e3600c48b332c
Anushka <ay0496618@gmail.com>
2026-08-03 14:55:25 +0530

    Add files via upload

---
+++ Class9-programs/Expressions1.java
@@ -0,0 +1,20 @@
+// A program to find and display the value of some expressions.
+import java.util.*;
+public class Expressions1 {
+    public static void main(String[] args) {
+        try(Scanner in=new Scanner(System.in)) {
+        System.out.print("Enter the value of x: ");
+        double x=in.nextDouble();
+        System.out.print("Enter the value of a: ");
+        double a=in.nextDouble();
+        System.out.print("Enter the value of b: ");
+        double b=in.nextDouble();
+        System.out.print("Enter the value of c: ");
+        double c=in.nextDouble();
+        double y=((x+3)/6)-((2*x+5)/3);
+        double d=(a*a + b*b + c*c)/(a*b*c);
+        System.out.println("The answer of 1st expression is " + y);
+        System.out.println("The answer of 2nd expression is " + d);
+        }
+    }
+}
