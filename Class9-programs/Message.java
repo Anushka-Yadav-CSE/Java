4cb3f4e741b560090050e0f0aa8b21c7f11badee
Anushka <ay0496618@gmail.com>
2026-08-18 08:30:36 +0530

    Add files via upload

---
+++ Class9-programs/Message.java
@@ -0,0 +1,10 @@
+// A program to print a message on the screen 10 times.
+public class Message {
+    public static void main(String[] args) {
+        // For loop
+        for (int i = 0; i < 10; i++) {
+            // Output
+            System.out.println("Happy Birthday!!!");
+        }
+    }
+}
