060ffbbeffac618ca7d092903d4b23763e55d8d7
Anushka <ay0496618@gmail.com>
2026-08-18 08:31:20 +0530

    Add files via upload

---
+++ Class9-programs/NaturalNumbers.java
@@ -0,0 +1,12 @@
+// A program to print the natural numbers from 1 to 10.
+public class NaturalNumbers {
+    public static void main(String[] args) {
+        // Output
+        System.out.println("Natural numbers from 1 to 10: ");
+        // For loop
+        for (int i = 1; i <= 10; i++) {
+            // Output
+            System.out.println(i);
+        }
+    }
+}
