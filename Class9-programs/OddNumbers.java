9a88708afcbae4e29eda9053181c8bfa572a980b
Anushka <ay0496618@gmail.com>
2026-08-20 23:33:12 +0530

    Add files via upload

---
+++ Class9-programs/OddNumbers.java
@@ -0,0 +1,21 @@
+/* A program to find and display all the odd numbers from 1 to 10 by using the continue in a 
+while loop.*/
+public class OddNumbers {
+    public static void main(String[] args) {
+        int a = 0;
+        // Output
+        System.out.println("All odd numbers from 1 to 10 are: ");
+        // do-while loop
+        do {
+            // Calculation
+            a++;
+            // Condition
+            if (a % 2 == 0) {
+                continue;
+            }
+            // Output
+            System.out.println(a);            
+            }
+            while (a <= 9);
+    }
+}
