7f259315e175f6bd56614ca558c5e71afb4598c7
Anushka <ay0496618@gmail.com>
2026-08-21 22:53:24 +0530

    Add files via upload

---
+++ Class9-programs/EvenNumbers.java
@@ -0,0 +1,21 @@
+/* A program to find and display all the even numbers from 1 to 10 by using the continue statement
+  in a do-while loop.*/
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int a = 0;
+        // Output
+        System.out.println("All even numbers from 1 to 10 are: ");
+        // do-while loop
+        do {
+            // Calculation
+            a++;
+            // Condition
+            if (a % 2 != 0) {
+                continue;
+            }
+            // Output
+            System.out.println(a);
+        }
+        while (a <= 10);
+    }
+}
