e4997c7e4d60f6a40cdaab6442d3aecd90bed12e
Anushka <ay0496618@gmail.com>
2026-08-23 06:06:36 +0530

    Add files via upload

--- Class9-programs/Natural.java
+++ Class9-programs/Natural.java
@@ -1,4 +1,19 @@
-the faint image of my lovesick brother is flashing beforemmyde 4package
-
-
-
+// A program to display the sum of any 10 natural numbers.
+import java.util.*;
+public class Natural {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            int s = 0;
+            // for loop
+             for (int i = 0; i < 10; i++) {
+                // Input
+                System.out.println("Enter the natural number: ");
+                int n = in.nextInt();
+                // Calculation
+                s = s + n;                
+            }
+            // Output
+            System.out.println("Sum of any ten natural numbers is: " + s);
+        }
+    }
+}
