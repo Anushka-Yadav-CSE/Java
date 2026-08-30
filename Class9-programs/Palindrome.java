e50f4affeceeca68248d39d7e977e8df50f9ed00
Anushka-Yadav-CSE <ay0496618@gmail.com>
2026-08-30 09:20:42 +0530

    Added Palindrome program

--- Class9-programs/Palindrome.java
+++ Class9-programs/Palindrome.java
@@ -4,7 +4,27 @@ public class Palindrome {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             // Input
-            System.out.print("Enter the ");
+            int b, c = 0;
+            System.out.print("Enter the number: ");
+            int a = in.nextInt();
+            int d = a;
+            // do while loop
+            do {
+                // Calculation
+                b = a % 10;
+                c = c * 10 + b;
+                a = a / 10;
+            }
+            while (a != 0);
+            // Condition
+            if (c == d) {
+                // Output
+                System.out.println("The number is a palindrome number.");
+            }
+            else {
+                // Output
+                System.out.println("The number is not a palindrome number.");
+            }
         }
     }
 }
