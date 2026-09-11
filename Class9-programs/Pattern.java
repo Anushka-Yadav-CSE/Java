990edf6c7d552635e30507c9ba7cd2eb82708508
Anushka <312159153+Anushka-Yadav-CSE@users.noreply.github.com>
2026-09-11 20:30:52 +0530

    Added Pattern Program

--- Class9-programs/Pattern.java
+++ Class9-programs/Pattern.java
@@ -1,13 +1,20 @@
 // A program to print a pattern of numbers in a stairs format.
+// The Pattern is:
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
 public class Pattern {
     public static void main(String[] args) {
         // Loop through numbers from 1 to 5 to create the pattern.
         for (int i = 1; i <= 5; i++) {
-            // Inner loop to print the current number 'i' for 'i' times.
+            // Inner loop to print the current number 'j' for 'i' times.
             for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
-    }
+    } 
 }
