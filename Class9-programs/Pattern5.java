196c4d18f4c0c172b8e5832360f7d6b602e65c67
Anushka <312159153+Anushka-Yadav-CSE@users.noreply.github.com>
2026-09-12 22:15:05 +0530

    Added Pattern5 Program

--- Class9-programs/Pattern5.java
+++ Class9-programs/Pattern5.java
@@ -1,7 +1,14 @@
 // A program to print the pattern.
+// The Pattern is:
+// 9 7 5 3 1
+// 9 7 5 3
+// 9 7 5
+// 9 7
+// 9
+
 public class Pattern5 {
     public static void main(String[] args) {
-        System.out.println("The pattern is");
+        System.out.println("The pattern is:");
         // for loop to iterate through the rows.
         for (int i = 1; i <= 9; i += 2) {
             // for loop to iterate through the columns.
